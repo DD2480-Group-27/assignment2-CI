cf267adbf26f5df7d54089e98e798d53f8dc8332
KristinRosen <kristin.maria.rosen@gmail.com>
2025-02-06 11:32:44 +0100

    Feat: cloning the repo in Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,15 @@
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
- 
+
+import java.io.File;
 import java.io.IOException;
  
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 
 /** 
  Skeleton of a ContinuousIntegrationServer which acts as webhook
@@ -29,17 +32,33 @@ public class Main extends AbstractHandler
         // here you do all the continuous integration tasks
         // for example
         // 1st clone your repository
-        // 2nd compile the code
+        
+        String repo = "https://github.com/DD2480-Group-27/assignment2-CI.git";
 
-        response.getWriter().println("CI job done");
+       try {
+            System.out.println("Cloning repository from " + repo);
+            Git.cloneRepository()
+                    .setURI(repo)
+                    .setDirectory(null)
+                    .call();
+            System.out.println("Repository cloned successfully.");
+        } catch (GitAPIException e) {
+            e.printStackTrace();
+        }
+
+        // 2nd compile the code
+       
     }
  
     // used to start the CI server in command line
     public static void main(String[] args) throws Exception
     {
+        
         Server server = new Server(8080);
         server.setHandler(new Main()); 
         server.start();
         server.join();
+        
+      
     }
 }
