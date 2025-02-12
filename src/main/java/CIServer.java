11b7fec5b66411606922e15931a2ff3b1e1340d8
Edgar WOLFF <edgar.wolff@epfl.ch>
2025-02-12 13:48:30 +0100

    refact: Change code structure to enable efficient unit testing
    Refactored the CIServer.java file in adding an imbricated calss to parse the needed information from webhook JSON data
    Refactored the CIServerTest.java file to match the new implementation in CIServer.java

--- src/main/java/CIServer.java
+++ src/main/java/CIServer.java
@@ -1,51 +1,51 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.ResetCommand;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import jakarta.servlet.ServletException;
-import org.eclipse.jgit.api.ResetCommand;
-import org.json.JSONObject;
-import org.eclipse.jgit.api.Git;
 
 public class CIServer extends AbstractHandler {
 
     /**
      * Handles incoming HTTP requests for the CI server.
-     *
+     * <p>
      * This method processes HTTP requests by reading the JSON payload from the request body,
      * extracting relevant data such as the branch reference, repository URL, and commit hash.
      * It then clones the specified repository to a temporary directory and resets it to the
      * exact state of the specified commit. The method responds to the client with a confirmation
      * message indicating that the server has received the request.
      *
-     * @param target       The target URL of the request.
-     * @param baseRequest  The base request object, which provides access to request and response details.
-     * @param request      The HttpServletRequest containing the client's request data.
-     * @param response     The HttpServletResponse used to send a response back to the client.
+     * @param target      The target URL of the request.
+     * @param baseRequest The base request object, which provides access to request and response details.
+     * @param request     The HttpServletRequest containing the client's request data.
+     * @param response    The HttpServletResponse used to send a response back to the client.
      * @throws IOException      If an I/O error occurs during request processing.
      * @throws ServletException If the request cannot be handled due to a servlet error.
-     *
-     * Process Overview:
-     * 1. Sets the response content type to HTML and the status to OK.
-     * 2. Reads the JSON payload from the request body.
-     * 3. Parses the JSON data to extract the branch reference, repository URL, and commit hash.
-     * 4. Converts the branch reference into a proper branch name.
-     * 5. Calls the `cloneRepo` method to clone the repository and reset it to the specified commit.
-     * 6. Use the cloned repository for code validation process.
-     * 7. Sends a confirmation message back to the client indicating successful handling of the request.
+     *                          <p>
+     *                          Process Overview:
+     *                          1. Sets the response content type to HTML and the status to OK.
+     *                          2. Reads the JSON payload from the request body.
+     *                          3. Parses the JSON data to extract the branch reference, repository URL, and commit hash.
+     *                          4. Converts the branch reference into a proper branch name.
+     *                          5. Calls the `cloneRepo` method to clone the repository and reset it to the specified commit.
+     *                          6. Use the cloned repository for code validation process.
+     *                          7. Sends a confirmation message back to the client indicating successful handling of the request.
      */
     public void handle(String target,
                        Request baseRequest,
                        HttpServletRequest request,
                        HttpServletResponse response)
-        throws IOException, ServletException
-    {
+            throws IOException, ServletException {
 //        System.out.println("In handle process");
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
@@ -53,122 +53,41 @@ public class CIServer extends AbstractHandler {
 
         // Read the JSON payload
 //        System.out.println("Reading the payload");
-        StringBuilder payload = new StringBuilder();
+        StringBuilder payloadBuilder = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(
                 new InputStreamReader(request.getInputStream(), StandardCharsets.UTF_8))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                payload.append(line).append("\n");
+                payloadBuilder.append(line).append("\n");
             }
         }
 
+        WebhookJSONAnalyser payloadAnalyser = null;
         try {
-            JSONObject json = new JSONObject(payload.toString());
-            // Get the branch name
-            String ref = json.getString("ref");
-            // Get the repo URL
-            String htmlUrl = json.getJSONObject("repository").getString("html_url");
-            // Get the commit hash id
-            String commitHash = json.getJSONObject("head_commit").getString("id");
-            // Parse the ref into a proper branch name
-            String branchName = ref.replace("refs/heads/", "");
+            payloadAnalyser = new WebhookJSONAnalyser(payloadBuilder.toString());
 
-            System.out.println("The received webhook data:");
-            System.out.println("--------------------------------------------------");
-            System.out.println("Ref: " + ref);
-            System.out.println("Branch Name: "+ branchName);
-            System.out.println("Repo URL: " + htmlUrl);
-            System.out.println("Commit Hash: "+ commitHash);
-            System.out.println("--------------------------------------------------");
-            // Store the path to the cloned repo
-            String codePath =  cloneRepo(htmlUrl, commitHash, branchName );
+            var repoPath = payloadAnalyser.repoPath;
 
-            // Code Validation
+            // TODO call verification and retrieve either compilation failure output
+            //  or test results if compilation succeeded
 
-            // After
 
-        } catch (Exception e) {
-            System.out.println("Error parsing JSON payload: " + e.getMessage());
+            var commitHash = payloadAnalyser.commitHash;
+            var commitMail = payloadAnalyser.commitMail;
+            var commitMessage = payloadAnalyser.commitMessage;
+            var commitRef = payloadAnalyser.commitRef;
+            var commitBranch = payloadAnalyser.commitBranch;
+            var commitAuthor = payloadAnalyser.commitAuthor;
+            var repoURL = payloadAnalyser.repoURL;
+
+            // TODO call email notification
+
+        } catch (RuntimeException e) {
+            System.err.println("Failed to parse payload from last request");
         }
 
         response.getWriter().println("The CI server says 'Hello!'");
     }
-
-    /**
-     * Clones a Git repository to a temporary directory and resets it to a specific commit.
-     *
-     * This method creates a unique directory for the specified commit within a base
-     * temporary directory. If a directory for the commit already exists, it is deleted
-     * before the clone operation. After cloning, the method checks out the specified
-     * branch and performs a hard reset to ensure the working directory matches the
-     * exact state of the specified commit.
-     *
-     * @param repoURL    The URL of the Git repository to clone.
-     * @param commitHash The hash of the commit to which the repository should be reset.
-     * @param branchName The name of the branch to check out after cloning.
-     * @return The absolute path of the cloned repository in the temporary directory.
-     *         Returns null if an error occurs during the cloning process.
-     */
-    private String cloneRepo(String repoURL, String commitHash, String branchName) {
-        try {
-            // Get system temp directory in a platform-independent way
-            File tempDir = new File(System.getProperty("java.io.tmpdir"));
-            File baseDir = new File(tempDir, "dd2480-builds");
-            if (!baseDir.exists()) {
-                baseDir.mkdir();
-            }
-
-            // Create a unique directory for this build using the commit hash
-            String buildDir = "build-" + commitHash;
-            File repoDir = new File(baseDir, buildDir);
-            //if the directory exists for a commit hash, then delete it
-            if(repoDir.exists()){
-               deleteDirectory(repoDir);
-            }
-            //then create a new empty directory
-            repoDir.mkdir();
-
-            // Clone the repository
-            Git git = Git.cloneRepository()
-                    .setURI(repoURL)
-                    .setDirectory(repoDir)
-                    .call();
-            // Checkout the right branch
-            git.checkout().setName(branchName).call();
-            // Hard reset to the specific commit
-            git.reset()
-                    .setMode(ResetCommand.ResetType.HARD)
-                    .setRef(commitHash)
-                    .call();
-            git.close();
-            System.out.println("Repository cloned and hard reset to commit " + commitHash);
-            return repoDir.getAbsolutePath();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    /**
-     * Helper Fucntion:
-     * this function aims to delete a given directory recursively
-     * @param directory
-     */
-    private void deleteDirectory(File directory) {
-        if (directory.isDirectory()) {
-            // List all files and subdirectories
-            File[] files = directory.listFiles();
-            if (files != null) {
-                for (File file : files) {
-                    // Recursively delete subdirectories and files
-                    deleteDirectory(file);
-                }
-            }
-        }
-        // delete the directory itself
-        directory.delete(); }
-
 
     // used to start the CI server in command line
     public static void main(String[] args) throws Exception {
@@ -176,5 +95,187 @@ public class CIServer extends AbstractHandler {
         server.setHandler(new CIServer());
         server.start();
         server.join();
+    }
+
+    /**
+     * Public class intended to fetch relevant data from JSON webhook request
+     */
+    public static class WebhookJSONAnalyser {
+
+        private final String repoPath;
+        private final String repoURL;
+        private final String commitHash;
+        private final String commitMail;
+        private final String commitMessage;
+        private final String commitRef;
+        private final String commitBranch;
+        private final String commitAuthor;
+
+        /**
+         * Class constructor will parse the given JSON String and set its parameters to the relevant values
+         *
+         * @param payload a String containing a JSON encoded GitHub webhook payload
+         */
+        public WebhookJSONAnalyser(String payload) {
+            try {
+                JSONObject json = new JSONObject(payload);
+                // Get the branch name
+                this.commitRef = json.getString("ref");
+                // Get the repo URL
+                this.repoURL = json.getJSONObject("repository").getString("html_url");
+                // Get the commit hash id
+                this.commitHash = json.getJSONObject("head_commit").getString("id");
+                // Parse the ref into a proper branch name
+                this.commitBranch = commitRef.replace("refs/heads/", "");
+
+                System.out.println("The received webhook data:");
+                System.out.println("--------------------------------------------------");
+                System.out.println("Ref: " + commitRef);
+                System.out.println("Branch Name: " + commitBranch);
+                System.out.println("Repo URL: " + repoURL);
+                System.out.println("Commit Hash: " + commitHash);
+                System.out.println("--------------------------------------------------");
+
+                // Store the path to the cloned repo
+                this.repoPath = cloneRepo(repoURL, commitHash, commitBranch);
+                this.commitMail = "dumb@email.com";     //TODO set with the commit's email address
+                this.commitAuthor = "author";           //TODO set with author
+                this.commitMessage = "message";         //TODO set with commit message
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);          //TODO make maybe a better handling (printing a little bit of information about the fail if possible (idk))
+            }
+
+
+        }
+
+        /**
+         * Clones a Git repository to a temporary directory and resets it to a specific commit.
+         * <p>
+         * This method creates a unique directory for the specified commit within a base
+         * temporary directory. If a directory for the commit already exists, it is deleted
+         * before the clone operation. After cloning, the method checks out the specified
+         * branch and performs a hard reset to ensure the working directory matches the
+         * exact state of the specified commit.
+         * </p>
+         *
+         * @param repoURL    The URL of the Git repository to clone.
+         * @param commitHash The hash of the commit to which the repository should be reset.
+         * @param branchName The name of the branch to check out after cloning.
+         * @return The absolute path of the cloned repository in the temporary directory.
+         * Returns null if an error occurs during the cloning process.
+         */
+        private String cloneRepo(String repoURL, String commitHash, String branchName) {
+            try {
+                // Get system temp directory in a platform-independent way
+                File tempDir = new File(System.getProperty("java.io.tmpdir"));
+                File baseDir = new File(tempDir, "dd2480-builds");
+                if (!baseDir.exists()) {
+                    baseDir.mkdir();
+                }
+
+                // Create a unique directory for this build using the commit hash
+                String buildDir = "build-" + commitHash;
+                File repoDir = new File(baseDir, buildDir);
+                //if the directory exists for a commit hash, then delete it
+                if (repoDir.exists()) {
+                    deleteDirectory(repoDir);
+                }
+                //then create a new empty directory
+                repoDir.mkdir();
+
+                // Clone the repository
+                Git git = Git.cloneRepository()
+                        .setURI(repoURL)
+                        .setDirectory(repoDir)
+                        .call();
+                // Checkout the right branch
+                git.checkout().setName(branchName).call();
+                // Hard reset to the specific commit
+                git.reset()
+                        .setMode(ResetCommand.ResetType.HARD)
+                        .setRef(commitHash)
+                        .call();
+                git.close();
+                System.out.println("Repository cloned and hard reset to commit " + commitHash);
+                return repoDir.getAbsolutePath();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        private void deleteDirectory(File directory) {
+            if (directory.isDirectory()) {
+                // List all files and subdirectories
+                File[] files = directory.listFiles();
+                if (files != null) {
+                    for (File file : files) {
+                        // Recursively delete subdirectories and files
+                        deleteDirectory(file);
+                    }
+                }
+            }
+            // delete the directory itself
+            directory.delete();
+        }
+
+        /**
+         * @return a String containing the commit author's email
+         */
+        public String getCommitMail() {
+            return commitMail;
+        }
+
+        /**
+         * @return a String representing the absolut path to where the repo has been cloned and set to the right version
+         */
+        public String getRepoPath() {
+            return repoPath;
+        }
+
+        /**
+         * @return a String containing the commit hash
+         */
+        public String getCommitHash() {
+            return commitHash;
+        }
+
+        /**
+         * @return a String containing the commit message
+         */
+        public String getCommitMessage() {
+            return commitMessage;
+        }
+
+        /**
+         * @return a String containing the commit reference
+         */
+        public String getCommitRef() {
+            return commitRef;
+        }
+
+        /**
+         * @return a String containing the commit branch
+         */
+        public String getCommitBranch() {
+            return commitBranch;
+        }
+
+        /**
+         * @return a String containing the URL
+         */
+        public String getRepoURL() {
+            return repoURL;
+        }
+
+        /**
+         * @return a String containing the commit author's username
+         */
+        public String getCommitAuthor() {
+            return commitAuthor;
+        }
+
     }
 }
--- src/test/java/CIServerTest.java
+++ src/test/java/CIServerTest.java
@@ -1,26 +1,10 @@
-import org.junit.*;
-import org.eclipse.jetty.server.Server;
-import org.json.JSONObject;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStreamReader;
-import java.io.BufferedOutputStream;
-import java.nio.charset.StandardCharsets;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.stream.Collectors;
-import static org.junit.Assert.*;
 
 public class CIServerTest {
-    // Creating a local server
-    private static Server server;
-    private static String repoPath;
-    private static String commitHash;
-    private static boolean isServerStartedBeforeTest; // true: port in used; false; port not in used
 
     @BeforeClass
     /**
@@ -28,116 +12,46 @@ public class CIServerTest {
      */
     public static void setUp() throws Exception {
         // Run the setup script
-        ProcessBuilder builder = new ProcessBuilder("sh", "src/test/java/server_communication/server_setup.sh");
+        ProcessBuilder builder = new ProcessBuilder("sh", "resources/server_setup.sh");
         builder.inheritIO();
         Process process = builder.start();
         int exitCode = process.waitFor();
         if (exitCode != 0) {
             throw new IOException("Failed to set up test repository");
         }
-//        System.out.println("Server and repo successfully added");
-
-        // Read the commit hash from file
-        commitHash = Files.readString(Path.of("commit_hash.txt")).trim();
-//        System.out.println("Commit Hash: "+commitHash);
-        // Read the repo path from file
-        repoPath = Files.readString(Path.of("repo_path.txt")).trim();
-//        System.out.println("Repo Path: "+ repoPath);
-
-        //Check if the port is in used already
-        if(!isPortInUse(8027)){ //if port not in used
-            server = new Server(8027);
-            server.setHandler(new CIServer());
-            server.start();
-        }
-        Thread.sleep(500);
-    }
-
-    /**
-     * Helper Funciton
-     * this funciton aims to check if a port is in used for starting up a server.
-     * if the port is in used, the isServerStartedBeforeTest will be set accordingly
-     * the variable will be used to determine if the server needs to be stopped after the test.
-     * @param port
-     * @return
-     */
-    public static boolean isPortInUse(int port) {
-        try (ServerSocket socket = new ServerSocket(port)) {
-            isServerStartedBeforeTest = false;
-            return false; // Port is not in use
-        } catch (IOException e) {
-            isServerStartedBeforeTest = true;
-            return true; // Port is in use
-        }
     }
 
     /**
      * After the test finished, the server is stopped
+     *
      * @throws Exception
      */
     @AfterClass
     public static void tearDown() throws Exception {
-        // if server has already been started before the test,
-        // the server should not be stopped here in the test
-       if(!isServerStartedBeforeTest) {
-           server.stop();
-       }
         // Clean up
         ProcessBuilder cleanupBuilder = new ProcessBuilder("rm", "-rf", "test-repo", "commit_hash.txt", "repo_path.txt");
         cleanupBuilder.start().waitFor();
-        System.out.println("Server stopped");
     }
 
     /**
      * This test aims to test the following:
      * 1. The data retrieving from JSON payload through a POST
      * 2. The cloning of repo in a tmp directory
+     *
      * @throws Exception
      */
     @Test
     public void testCloneRepository() throws Exception {
         // Prepare the JSON payload (customised)
-        String jsonPayload = new JSONObject()
-                .put("ref", "refs/heads/feature-branch")
-                .put("repository", new JSONObject()
-                        //Paste the file location to your local repo
-                        .put("html_url", "file://"+repoPath)) // Update this path
-                .put("head_commit", new JSONObject()
-                        // To get the commit hash, cd to your repo, then do git log, then paste it below
-                        .put("id", commitHash))
-                .toString();
 
-        // Send the POST request
-        URL url = new URL("http://localhost:8027/");
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("POST");
-        connection.setDoOutput(true);
-        connection.setRequestProperty("Content-Type", "application/json");
-//        System.out.println("Test POST request sent!");
-        try (BufferedOutputStream out = new BufferedOutputStream(connection.getOutputStream())) {
-            out.write(jsonPayload.getBytes(StandardCharsets.UTF_8));
-            out.flush(); // Ensure data is sent
-        }
+        var jsonPayload = "";  // TODO set with json data test sample (reading file from resources)
 
-    // Check the response code
-        int responseCode = connection.getResponseCode();
-        System.out.println("Response Code: " + responseCode);
+        var webhookHandler = new CIServer.WebhookJSONAnalyser(jsonPayload);
 
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            // Read the response
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8))) {
-                String response = reader.lines().collect(Collectors.joining("\n"));
-//                System.out.println("Response from server: " + response);
-                assertEquals("The CI server says 'Hello!'", response);
-            }
-        } else {
-            System.out.println("Failed to send POST request. Response Code: " + responseCode);
-        }
-        // Wait a bit for processing
-        Thread.sleep(1000);
+        // TODO try the getters to check values and if repo is indeed cloned at right location
 
-        // Check if the repository was cloned
-        File clonedRepoDir = new File(System.getProperty("java.io.tmpdir"), "dd2480-builds/build-"+commitHash);
-        assertTrue(clonedRepoDir.exists());
+        // webhookHandler.getCommit*();         // compare value for each
+        // webhookHandler.getRepoPath();        // check value validity
+
     }
 }
