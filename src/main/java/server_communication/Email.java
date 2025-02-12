2899c276279e508acfb2ecc6ce7742ee93af5e76
KristinRosen <kristin.maria.rosen@gmail.com>
2025-02-12 21:04:54 +0100

    Kristin/feat/send email (Closes #18) (#22)
    * Feat: can send email through mailtrap demo
    
    * Feat: Sending basic email to addresses specified in code, with the help of SendGrid. (solves #18)
    * Added dependencies for email sending.
    * Added Email.java file.
    * Emails are sent through Kristin's SendGrid account.
    * Recipient addresses are hardcoded in Email.java
    * Message content is hardcoded in Email.java
    
    * Feat: created constructor for Email and a Send function. Moved function calling to Main.java
    
    * Refactor: Removed Main.java bc misunderstanding of structure

---
+++ src/main/java/server_communication/Email.java
@@ -0,0 +1,67 @@
+package server_communication;
+
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
+import java.util.Properties;
+
+public class Email {
+
+    String to;
+    String from;
+    String username;
+    String password;
+    String host;
+
+    public Email(String recipient){
+        this.to = recipient;
+        this.from = "testenkristest@gmail.com";
+        this.username = "apikey";
+        this.password = "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";
+        this.host = "smtp.sendgrid.net";
+    }
+
+    public void Send(String subject, String content){
+        // configure SMTP details
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", "587");
+
+        // create the mail Session object
+        Session session = Session.getInstance(props,
+        new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+
+        try {
+            // create a MimeMessage object
+            Message message = new MimeMessage(session);
+            // set From email field
+            message.setFrom(new InternetAddress(from));
+            // set To email field
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+            // set email subject field
+            message.setSubject(subject);
+            // set the content of the email message
+            message.setText(content);
+    
+            // send the email message
+            Transport.send(message);
+    
+            } catch (MessagingException e) {
+                throw new RuntimeException(e);
+            }
+    }
+   
+
+   
+}
+
+
+        
