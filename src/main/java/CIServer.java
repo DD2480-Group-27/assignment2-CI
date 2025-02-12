25682f446842b02a309edb57a0563613357780bb
Edgar WOLFF <edgar.wolff@epfl.ch>
2025-02-12 20:45:29 +0100

    refac: Complete code adaptation for merging
    Fixed the calls from main handling method towards code_verification package

--- src/main/java/CIServer.java
+++ src/main/java/CIServer.java
@@ -1,30 +1,19 @@
-
 import code_verification.CodeVerifier;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-import server_communication.WebhookJSONAnalyser;
-
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.ResetCommand;
-import org.eclipse.jgit.api.errors.GitAPIException;
+import server_communication.WebhookJSONAnalyser;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 
 
 public class CIServer extends AbstractHandler {
-
 
     /**
      * Handles incoming HTTP requests for the CI server.
@@ -34,22 +23,22 @@ public class CIServer extends AbstractHandler {
      * It then clones the specified repository to a temporary directory and resets it to the
      * exact state of the specified commit. The method responds to the client with a confirmation
      * message indicating that the server has received the request.
-     *
+     * <p>
+     * Process Overview:
+     * 1. Sets the response content type to HTML and the status to OK.
+     * 2. Reads the JSON payload from the request body.
+     * 3. Parses the JSON data to extract the branch reference, repository URL, and commit hash.
+     * 4. Converts the branch reference into a proper branch name.
+     * 5. Calls the `cloneRepo` method to clone the repository and reset it to the specified commit.
+     * 6. Use the cloned repository for code validation process.
+     * 7. Sends a confirmation message back to the client indicating successful handling of the request.
+     * </p>
      * @param target      The target URL of the request.
      * @param baseRequest The base request object, which provides access to request and response details.
      * @param request     The HttpServletRequest containing the client's request data.
      * @param response    The HttpServletResponse used to send a response back to the client.
      * @throws IOException      If an I/O error occurs during request processing.
      * @throws ServletException If the request cannot be handled due to a servlet error.
-     *                          <p>
-     *                          Process Overview:
-     *                          1. Sets the response content type to HTML and the status to OK.
-     *                          2. Reads the JSON payload from the request body.
-     *                          3. Parses the JSON data to extract the branch reference, repository URL, and commit hash.
-     *                          4. Converts the branch reference into a proper branch name.
-     *                          5. Calls the `cloneRepo` method to clone the repository and reset it to the specified commit.
-     *                          6. Use the cloned repository for code validation process.
-     *                          7. Sends a confirmation message back to the client indicating successful handling of the request.
      */
     public void handle(String target,
                        Request baseRequest,
@@ -70,69 +59,39 @@ public class CIServer extends AbstractHandler {
             }
         }
 
-
-        WebhookJSONAnalyser payloadAnalyser = null;
-
         try {
-            payloadAnalyser = new WebhookJSONAnalyser(payloadBuilder.toString());
+            WebhookJSONAnalyser payloadAnalyser = new WebhookJSONAnalyser(payloadBuilder.toString());
+
+            // Code Validation
+            var codeVerifier = new CodeVerifier(payloadAnalyser.getRepoPath());
 
             var commitHash = payloadAnalyser.getCommitHash();
             var commitMail = payloadAnalyser.getCommitMail();
-            var commitMessage = payloadAnalyser.getCommitMessage(); // Is this neccesary?
-            var commitRef = payloadAnalyser.getCommitRef();         
-            var commitBranch = payloadAnalyser.getCommitBranch();   
-            var commitAuthor = payloadAnalyser.getCommitAuthor();   // Is this neccesary?
+            var commitMessage = payloadAnalyser.getCommitMessage();
+            var commitRef = payloadAnalyser.getCommitRef();
+            var commitBranch = payloadAnalyser.getCommitBranch();
+            var commitAuthor = payloadAnalyser.getCommitAuthor();
             var repoURL = payloadAnalyser.getRepoURL();
 
-            // Store the path to the cloned repo
-            String codePath = cloneRepo(repoURL, commitHash, commitBranch);
-
-            // Code Validation
-            var codeVerifier = new CodeVerifier(codePath);
-
-            
             // Verifies the code compilation and runs the associated tests if compilation is successful.
             // If the code fails to compile, retrieves the compilation output.
             // If the tests fail, retrieves the test result and output for further notification.
-            try {
-                if (codeVerifier.verifyCompilation()) {
-                    var testResult = codeVerifier.runTests();
-                    var testOutputXml = codeVerifier.getTestXml();
-                    // TODO  notification with test result if testResult != 0 also send testOutputXml
-                } else {
-                    var compilationOutput = codeVerifier.getCompilationOutput();
-                    // TODO  notification with compilation result
-                }
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                throw new IOException("Compilation process was interrupted", e);
+            if (codeVerifier.verifyCompilation()) {
+                var testResult = codeVerifier.runTests();
+                var testOutputXml = codeVerifier.getTestXml();
+                // TODO  notification with test result if testResult != 0 also send testOutputXml
+            } else {
+                var compilationOutput = codeVerifier.getCompilationOutput();
+                // TODO  notification with compilation result
             }
 
-
-            // TODO call email notification
-
+        } catch (InterruptedException e) {
+            System.err.println("Compilation or testing process was interrupted");
         } catch (RuntimeException e) {
             System.err.println("Failed to parse payload from last request");
         }
 
         response.getWriter().println("The CI server says 'Hello!'");
-    }
-    
-    private String cloneRepo(String repoURL, String commitHash, String branchName) throws IOException {
-        File localRepo = Files.createTempDirectory("ci-server-repo").toFile();
-        try {
-            Git git = Git.cloneRepository()
-                    .setURI(repoURL)
-                    .setDirectory(localRepo)
-                    .setBranch(branchName)
-                    .call();
-
-            git.reset().setMode(ResetCommand.ResetType.HARD).setRef(commitHash).call();
-            git.close();
-        } catch (GitAPIException e) {
-            throw new IOException("Failed to clone repository: " + e.getMessage(), e);
-        }
-        return localRepo.getAbsolutePath();
     }
 
     // used to start the CI server in command line
